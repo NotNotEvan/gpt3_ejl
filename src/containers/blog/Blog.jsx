f630a51d166f62e6bf851bb16baeceda9a6573e6
Evan Lovett <evanjameslovett@gmail.com>
2023-09-20 00:03:37 -0400

    Completed all containers and components

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Article } from '../../components';
+import { blog01, blog02, blog03, blog04, blog05 } from './imports'
 import './blog.css';
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='gpt3__blog section__padding' id='blog'>
+      <div className='gpt3__blog-heading'>
+        <h1 className='gradient__text'>
+        A lot is happening, 
+        We are blogging about it.</h1>
+      </div>
+      <div className='gpt3__blog-container'>
+        <div className='gpt3__blog-container_groupA'>
+          <Article imgUrl={blog01} date="Sep 26, 2021" title="From Zero to Chatbot: The Evolution of GPT Technology"/>
+        </div>
+        <div className='gpt3__blog-container_groupB'>
+          <Article imgUrl={blog02} date="Aug 30, 2021" title="Chat GPT: Bridging the Gap Between AI and Human Conversations"/>
+          <Article imgUrl={blog03} date="Aug 14, 2021" title="Decoding Chat GPT: The Future of AI-Powered Communication"/>
+          <Article imgUrl={blog04} date="Jul 19, 2021" title="Behind the Scenes: How Chat GPT Transforms Text into Talk"/>
+          <Article imgUrl={blog05} date="Jul 02, 2021" title="The Rise of Chat GPT: Revolutionizing Digital Interactions"/>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
 import './footer.css';
+import gpt3Logo from '../../assets/logo.svg'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='gpt3__footer section__padding'>
+      <div className='gpt3__footer-heading'>
+        <h1 className='gradient__text'>Do you want to step in to the future before others?</h1>
+      </div>
+
+      <div className='gpt3__footer-btn'>
+        <p>Request Early Access</p>
+      </div>
+      <div className='gpt3__footer-links'>
+        <div className='gpt3__footer-links_logo'>
+          <img src={gpt3Logo} alt='logo'/>
+          <p>San Francisco, California, All Rights Reserved</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Links</h4>
+          <p>Social Media</p>
+          <p>OpenAI</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Get in touch</h4>
+          <p>San Francisco, California</p>
+          <p>555-5555</p>
+          <p>info@openai.com</p>
+        </div>
+      </div>
+      <div className='gpt3__footer-copywright'>
+        <p>© 2021 GPT-3. All rights reserved.</p>
+      </div>
+    </div>
   )
 }
 
